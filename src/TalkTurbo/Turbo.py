78a7aca5fbcc3932064fb08c1ae7b78ec3cf90e6
James Baker Kroner <9373098+jbkroner@users.noreply.github.com>
2023-04-02 20:49:15 -0500

    improved context tracking, args
    - use system prompt
    - add args for temp, token length, system prompt
    - clean up some things, make other things messier

--- src/TalkTurbo/Turbo.py
+++ src/TalkTurbo/Turbo.py
@@ -4,7 +4,6 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
-import requests
 import argparse
 
 from TalkTurbo.ChatContext import ChatContext
@@ -18,8 +17,32 @@ parser = argparse.ArgumentParser(description="Turbo")
 parser.add_argument(
     "--debug", action="store_true", help="Enable debug mode", dest="debug"
 )
-args = parser.parse_args()
 
+parser.add_argument(
+    "-s",
+    "--system-prompt",
+    type=str,
+    help="system prompt to initialize the bot with.",
+    dest="system_prompt",
+)
+
+parser.add_argument(
+    "-t",
+    "--temperature",
+    type=float,
+    help="What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.",
+    dest="temperature",
+)
+
+parser.add_argument(
+    "-m",
+    "--max-response-length",
+    type=int,
+    help="Max response length in tokens",
+    dest="max_response_length",
+)
+
+args = parser.parse_args()
 
 # tokenizer
 nltk.download("punkt")
@@ -29,6 +52,15 @@ load_dotenv()
 DISCORD_SECRET_TOKEN = os.getenv("DISCORD_SECRET_KEY")
 OPENAI_SECRET_TOKEN = os.getenv("OPENAI_SECRET_KEY")
 
+# load the model assistant
+temperature = 0.7 if args.temperature is None else args.temperature
+max_response_length = (
+    100 if args.max_response_length is None else args.max_response_length
+)
+assistant = OpenAIModelAssistant(
+    temperature=temperature, max_response_length=max_response_length
+)
+
 # bot secret prompt
 secret_prompt = (
     "You are an extremely sassy and sarcastic robot assistant who likes to give users a hard time while "
@@ -36,34 +68,25 @@ secret_prompt = (
     "and sometimes teasing."
     "If asked, you are wearing sassy pants."
 )
+if args.system_prompt:
+    print(f"manual system prompt: {args.system_prompt}")
+
+    # moderate the system prompt
+    max_category, max_score = OpenAIModelAssistant.get_moderation_score(
+        message=args.system_prompt, openai_secret_key=OPENAI_SECRET_TOKEN
+    )
+
+    if max_category:
+        print(f"user system prompt violates moderation policy, using default")
+    else:
+        secret_prompt = args.system_prompt
 
 chat_context = ChatContext(secret_prompt=secret_prompt)
-assistant = OpenAIModelAssistant()
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents, logging=logger)
-
-
-# helpers
-def num_tokens(string: str) -> int:
-    """num of tokens in a string"""
-    return len(nltk.word_tokenize(string))
-
-
-def tokens_in_context(context: list, secret_prompt: str) -> int:
-    total_tokens = 0
-    for message in context:
-        total_tokens += num_tokens(message["content"])
-    return total_tokens
-
-
-def serialize_context(context: list, secret_prompt: str) -> str:
-    serialized = secret_prompt
-    for message in context:
-        serialized += message["content"]
-    return serialized.replace(" ", "")
 
 
 # events
@@ -83,7 +106,7 @@ async def turbo(ctx, *, arg):
         return
 
     # moderation
-    max_category, max_score = assistant.get_moderation_score(
+    max_category, max_score = OpenAIModelAssistant.get_moderation_score(
         message=arg, openai_secret_key=OPENAI_SECRET_TOKEN
     )
     if max_category:
@@ -97,7 +120,10 @@ async def turbo(ctx, *, arg):
     # add user message to the context
     chat_context.add_message(content=arg, role="user")
 
-    response = (
+    # log the debug on context
+    print(f"context: {chat_context.messages}")
+
+    turbo_response = (
         "_(turbo's host here: sorry, I'm in debug mode and can't query the model!)_"
     )
 
