3a3109cce3e07654b1b58d2729d8ef16cbf6f429
James Baker Kroner <9373098+jbkroner@users.noreply.github.com>
2023-04-06 23:01:57 -0500

    use --no-isolation
    we activate a venv earlier in the script.  we can shorten build time by not creating a new venv every time we build.

--- build.ps1
+++ build.ps1
@@ -30,7 +30,7 @@ if (Test-Path $requirements_file) {
 }
 
 Write-Output "### building TalkTurbo"
-python -m build
+python -m build --no-isolation
 
 Write-Output "uninstalling old versions"
 pip uninstall TalkTurbo
