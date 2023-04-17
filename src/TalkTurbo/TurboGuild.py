0f30fc5c26b5ff05385b883f2253e6e895855286
James Baker Kroner <9373098+jbkroner@users.noreply.github.com>
2023-04-16 21:50:32 -0500

    add metering to TurboGuild

--- src/TalkTurbo/TurboGuild.py
+++ src/TalkTurbo/TurboGuild.py
@@ -1,8 +1,9 @@
-from discord import Guild
-
+from typing import Tuple
 
 from ChatContext import ChatContext
 from TurboTier import TurboTier
+from TalkTurbo.Metering.Meter import Meter
+from TalkTurbo.Metering.TurboInteraction import TurboInteraction
 
 
 class TurboGuild:
@@ -12,3 +13,8 @@ class TurboGuild:
         self.messages_sent = 0
         self.tokens_used = 0
         self.turbo_tier = TurboTier.FREE
+        self.meter: Meter = Meter(guild_id=id)
+
+    def update_meter(self, interaction: TurboInteraction):
+        """add an interaction to this guilds meter"""
+        self.meter.add_interaction(interaction=interaction)
