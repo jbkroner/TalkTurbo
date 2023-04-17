e0ae64e310b2c794d6b8521f438138fccdbdc6ab
James Baker Kroner <9373098+jbkroner@users.noreply.github.com>
2023-04-16 21:51:37 -0500

    implement Meter.py

---
+++ src/TalkTurbo/Metering/Meter.py
@@ -0,0 +1,20 @@
+from TalkTurbo.Metering.TurboInteraction import TurboInteraction
+
+
+class Meter:
+    """
+    A Meter object tracks usage across a guild.
+    """
+
+    def __init__(self, guild_id: int) -> None:
+        self.guild_id = guild_id
+        self.interactions_log: dict = {}
+
+    def add_interaction(self, interaction: TurboInteraction):
+        # ensure that this interaction belongs to this guild
+        if interaction.guild_id != self.guild_id:
+            raise ValueError(
+                f"cannot add interaction guild_id ({interaction.guild_id}) to the meter for guild_id {self.guild_id}"
+            )
+
+        self.interactions_log[interaction.interaction_id] = interaction
