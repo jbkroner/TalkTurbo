e6e74321d06979d70772d8a59e5019bb3f5c71ba
James Baker Kroner <9373098+jbkroner@users.noreply.github.com>
2023-11-20 18:46:54 -0600

    add Messages module

---
+++ src/TalkTurbo/Messages.py
@@ -0,0 +1,61 @@
+from enum import Enum
+
+# api ref: https://platform.openai.com/docs/api-reference/chat/create
+
+class MessageRole(Enum):
+    SYSTEM = "system"
+    USER = "user"
+    ASSISTANT = "assistant"
+    TOOL = "tool"
+    FUNCTION = "function"
+
+class Message:
+    def __init__(self, role: MessageRole):
+        self.role = role
+
+class SystemMessage(Message):
+    def __init__(self, content: str, name: str=None):
+        """
+        args:
+            content: The contents of the system message
+            name: An optional name for the participant.
+                  Provides the model information to differentiate 
+                  between participants of the same role.
+        """
+        super().__init__(role=MessageRole.SYSTEM)
+        self.content = content
+        self.name = name
+
+class UserMessage(Message):
+    def __init__(self, content: str, name: str = None):
+        """
+        args:
+            content: The contents of the user message
+            name: An optional name for the participant.
+                  Provides the model information to differentiate 
+                  between participants of the same role.
+        """
+        super().__init__(role=MessageRole.USER)
+        self.content = content
+        self.name = name
+
+class AssistantMessage(Message):
+    def __init__(self, content: str, name: str = None):
+        """
+        args:
+            content: The contents of the assistant message
+            name: An optional name for the participant.
+                  Provides the model information to differentiate 
+                  between participants of the same role.
+        """
+        super().__init__(role=MessageRole.ASSISTANT)
+        self.content = content
+        self.name = name
+
+class FunctionMessage(Message):
+    def __init__(self, role: MessageRole):
+        raise NotImplementedError()
+
+class ToolMessage(Message):
+    def __init__(self, role: MessageRole):
+        raise NotImplementedError()
