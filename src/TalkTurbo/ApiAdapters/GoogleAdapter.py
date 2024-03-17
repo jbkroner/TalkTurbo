4dea3920f395d9e7b4c0e5e7ebf6212b6d4fb138
James Baker Kroner <9373098+jbkroner@users.noreply.github.com>
2024-03-17 14:24:55 -0500

    convert context

--- src/TalkTurbo/ApiAdapters/GoogleAdapter.py
+++ src/TalkTurbo/ApiAdapters/GoogleAdapter.py
@@ -3,7 +3,7 @@ from TalkTurbo.ApiAdapters.ApiAdapter import ApiAdapter
 import google.generativeai as genai
 
 from TalkTurbo.ChatContext import ChatContext
-from TalkTurbo.Messages import AssistantMessage, ContentMessage, MessageFactory
+from TalkTurbo.Messages import AssistantMessage, ContentMessage, MessageRole
 
 
 class GoogleAdapter(ApiAdapter):
@@ -12,10 +12,17 @@ class GoogleAdapter(ApiAdapter):
         self._api_token = api_token
         self._model_name = "gemini-pro"
         genai.configure(api_key=self._api_token)
-        print(genai)
         self._google_client = genai.GenerativeModel(model_name=self._model_name)
 
     def get_chat_completion(self, context: ChatContext) -> ContentMessage:
-        response = self._google_client.generate_content("hey there")
+        google_context = self.chat_context_to_google_format(context)
+        response = self._google_client.generate_content(google_context)
 
         return AssistantMessage(content=response.text)
+
+    def chat_context_to_google_format(self, context: ChatContext):
+        return [
+            {"role": message.role.value, "parts": [message.content]}
+            for message in context.messages
+            if message.role in [MessageRole.USER, MessageRole.ASSISTANT]
+        ]
