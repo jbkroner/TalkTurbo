d3318fcfda9c5d4b97702ea9ab6e37b232d470a3
James Baker Kroner <9373098+jbkroner@users.noreply.github.com>
2024-04-27 15:30:42 -0500

    add context pre-loader

---
+++ src/TalkTurbo/PreLoad.py
@@ -0,0 +1,41 @@
+import yaml
+
+from TalkTurbo.Messages import AssistantMessage, MessageRole, UserMessage
+
+
+def _validate_pre_load_data(pre_load_data: list):
+    if not isinstance(pre_load_data, list):
+        raise TypeError(
+            f"Pre-load data must be a list of dictionaries.  Data: {pre_load_data}"
+        )
+
+    for item in pre_load_data:
+        if not isinstance(item, dict):
+            raise TypeError(
+                f"Each item in the pre-load data must be a dictionary.  Data: {item}"
+            )
+        if (
+            MessageRole.USER.value not in item
+            or MessageRole.ASSISTANT.value not in item
+        ):
+            raise ValueError(
+                f"Each item in the pre-load data must have a user and system message.  Data: {item}"
+            )
+
+
+def get_pre_load_data(path: str) -> list:
+    with open(path, "r", encoding="utf-8") as file:
+        pre_load_data = yaml.safe_load(file)
+
+    _validate_pre_load_data(pre_load_data)
+
+    # convert to UserMessage and SystemMessage objects
+    pre_load_messages = []
+
+    for item in pre_load_data:
+        user_message = UserMessage(item[MessageRole.USER.value])
+        system_message = AssistantMessage(item[MessageRole.ASSISTANT.value])
+        pre_load_messages.append(user_message)
+        pre_load_messages.append(system_message)
+
+    return pre_load_messages
